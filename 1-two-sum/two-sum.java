99da777cf27d15aed7a663ff6bd8b6ec0b526563
muskanrawatt2012-dotcom <muskan.rawatt2012@gmail.com>
2026-07-07 16:13:50 +0530

    Time: 2 ms (99.33%) | Memory: 47.1 MB (58.01%) - LeetSync

---
+++ 1-two-sum/two-sum.java
@@ -0,0 +1,17 @@
+
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> seen = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int remaining = target - nums[i];
+            if (seen.containsKey(remaining)) {
+                return new int[] { seen.get(remaining), i };
+            }
+            seen.put(nums[i], i);
+        }
+        return new int[] {};
+    }
+}
+        
+ 
