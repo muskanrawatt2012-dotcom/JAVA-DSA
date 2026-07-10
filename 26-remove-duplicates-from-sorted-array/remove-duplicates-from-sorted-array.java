a1022a10e2095140a9bc08a2367c9bde3b6cc5d2
muskanrawatt2012-dotcom <muskan.rawatt2012@gmail.com>
2026-07-10 20:42:06 +0530

    Time: 0 ms (100.00%) | Memory: 46.8 MB (24.24%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        
+        int writeIndex = 1;
+        
+        for (int readIndex = 1; readIndex < nums.length; readIndex++) {
+            if (nums[readIndex] != nums[readIndex - 1]) {
+                nums[writeIndex] = nums[readIndex];
+                writeIndex++;
+            }
+        }
+        
+        return writeIndex;
+    }
+}
