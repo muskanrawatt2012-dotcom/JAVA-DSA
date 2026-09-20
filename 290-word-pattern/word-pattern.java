176d35bb3b5f4a40ed0cc67c62b1ef3157ce17a7
muskanrawatt2012-dotcom <muskan.rawatt2012@gmail.com>
2026-09-20 20:08:06 +0530

    Time: 1 ms (83.87%) | Memory: 42.8 MB (55.58%) - LeetSync

---
+++ 290-word-pattern/word-pattern.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+        if (pattern.length() != words.length) {
+            return false;
+        }
+
+        Map<Character, String> charToWord = new HashMap<>();
+        Map<String, Character> wordToChar = new HashMap<>();
+
+        for (int i = 0; i < pattern.length(); i++) {
+            char c = pattern.charAt(i);
+            String word = words[i];
+
+            if (charToWord.containsKey(c) && !charToWord.get(c).equals(word)) {
+                return false;
+            }
+            if (wordToChar.containsKey(word) && wordToChar.get(word) != c) {
+                return false;
+            }
+
+            charToWord.put(c, word);
+            wordToChar.put(word, c);
+        }
+
+        return true;
+    }
+}
